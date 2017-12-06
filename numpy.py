2fa5a660511549357736df12150970543a85bc54
abhiksark <abhiksark@gmail.com>
2017-12-06 22:26:02 +0530

    added numpy

---
+++ numpy.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 18 17:34:56 2017
+
+@author: abhik
+
+Basics functions for Numpy
+
+(DataPreprocessing)
+"""
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+a = np.array([0, 1, 2, 3])
+
+a.ndim
+
+a.shape
+
+# arange is an array-valued version of the built-in Python range function
+a = np.arange(10)
+
+#using linspace
+a = np.linspace(0, 1, 6) #start, end, number of points
+
+a = np.ones((3, 3))
+#The default data type is float for zeros and ones function
+
+"""
+array([[ 1.,  1.,  1.],
+       [ 1.,  1.,  1.],
+       [ 1.,  1.,  1.]])
+"""
+
+b = np.zeros((3, 3))
+
+
+c = np.eye(3)  #Return a 2-D array with ones on the diagonal and zeros elsewhere.
+
+a = np.diag([1, 2, 3, 4]) #construct a diagonal array.
+
+"""
+array([[1, 0, 0, 0],
+       [0, 2, 0, 0],
+       [0, 0, 3, 0],
+       [0, 0, 0, 4]])
+"""
+
+
+a = np.random.rand(4) 
+
+# array([ 0.57944059,  0.4826708 ,  0.66348841,  0.37141847])
+
+
+
+x = np.array([1, 2, 3, 4])
+np.sum(x)
+
+x.sum(axis=0)
+x.max()
+x.min()
+
+x.argmin()# index of minimum element
+x.argmax()
+
+a = np.array([[1, 2, 3], [4, 5, 6]])
+a.ravel() #Return a contiguous flattened array. A 1-D array, containing the elements of the input, is returned. A copy is made only if needed.
+
+a.T #Transpose 
+
+b = b.reshape((2, 3))
+
+a = np.array([[5, 4, 6], [2, 3, 2]])
+b = np.sort(a, axis=1)
--- pandas.py
+++ pandas.py
@@ -19,3 +19,31 @@ import pandas as pd
 dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 3].values
+
+
+#total number of rows and columns
+dataset.shape   
+
+#if you want to see last few rows then use tail command (default last 5 rows will print)
+dataset.tail()
+
+#slicing
+dataset[2:5]
+
+dataset.columns
+
+#priting particular column data 
+dataset.column
+dataset['column'] #dataset.column (both are same)
+dataset[['column1', 'column2']] #getting two or more column at once 
+
+
+
+dataset['column'].max()
+dataset['column'].min() 
+dataset['column'].describe()
+
+# select rows which has maximum value
+dataset[dataset.column == dataset.column.max()] 
+
+
