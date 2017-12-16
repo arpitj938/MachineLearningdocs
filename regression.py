9a1fe9b18a9da57b3bbd4b229b7397da14b8a098
Abhik <abhiksark@gmail.com>
2017-12-16 22:11:37 +0530

    Added Reinforcement

--- regression.py
+++ regression.py
@@ -7,6 +7,12 @@ Created on Tue Jul 18 15:43:05 2017
 regression basics
 
 """
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
 dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 3].values
---
+++ reinforcement.py
@@ -0,0 +1,27 @@
+
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = read.csv('Ads_CTR_Optimisation.csv')
+
+# Implementing Random Selection
+N = 10000
+d = 10
+ads_selected = integer(0)
+total_reward = 0
+for (n in 1:N) {
+  ad = sample(1:10, 1)
+  ads_selected = append(ads_selected, ad)
+  reward = dataset[n, ad]
+  total_reward = total_reward + reward
+}
+
+# Visualising the results
+hist(ads_selected,
+     col = 'blue',
+     main = 'Histogram of ads selections',
+     xlab = 'Ads',
+     ylab = 'Number of times each ad was selected')
