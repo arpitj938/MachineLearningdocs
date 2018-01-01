84a89730d0737d9992758ab74912f64dd82c311d
Abhik <abhiksark@gmail.com>
2018-01-01 23:23:34 +0530

    added tensorflow classification and regression

---
+++ tensorflow_basics.py
@@ -0,0 +1,103 @@
+import tensorflow as tf
+
+
+#tensors
+hello = tf.constant('Hello')
+type(hello) #tensorflow.python.framework.ops.Tensor
+
+world = tf.constant('World')
+result = hello + world
+
+result #<tf.Tensor 'add:0' shape=() dtype=string>
+
+with tf.Session() as sess:
+    result = sess.run(hello+world)
+
+result #b'HelloWorld'
+
+################################################################################################
+#Operations
+
+const = tf.constant(10)
+fill_mat = tf.fill((4,4),10)
+myzeros = tf.zeros((4,4))
+myones = tf.ones((4,4))
+myrandn = tf.random_normal((4,4),mean=0,stddev=0.5)
+myrandu = tf.random_uniform((4,4),minval=0,maxval=1)
+my_ops = [const,fill_mat,myzeros,myones,myrandn,myrandu]
+
+"""
+Tip: Interactive Session is Useful for Notebook Sessions Only
+"""
+################################################################################################
+"""
+Simple Neural Network 
+
+"""
+
+n_features = 10
+n_dense_neurons = 3
+
+
+# Placeholder for x
+x = tf.placeholder(tf.float32,(None,n_features))
+
+# Variables for w and b
+b = tf.Variable(tf.zeros([n_dense_neurons]))
+W = tf.Variable(tf.random_normal([n_features,n_dense_neurons]))
+
+xW = tf.matmul(x,W)
+z = tf.add(xW,b)
+
+# tf.nn.relu() or tf.tanh()
+a = tf.sigmoid(z)
+
+init = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+    sess.run(init)
+    
+    layer_out = sess.run(a,feed_dict={x : np.random.random([1,n_features])})
+
+################################################################################################
+""" 
+Regression Example 
+
+"""
+#Artifical Data
+x_data = np.linspace(0,10,10) + np.random.uniform(-1.5,1.5,10)
+y_label = np.linspace(0,10,10) + np.random.uniform(-1.5,1.5,10)
+
+plt.plot(x_data,y_label,'*')
+
+m = tf.Variable(0.39)
+b = tf.Variable(0.2)
+
+error = 0
+
+for x,y in zip(x_data,y_label):    
+    y_hat = m*x + b  #Our predicted value
+    error += (y-y_hat)**2 # The cost we want to minimize (we'll need to use an optimization function for the minimization!)
+
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
+train = optimizer.minimize(error)
+
+init = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+    sess.run(init)
+    epochs = 100
+    for i in range(epochs):
+        sess.run(train)
+    # Fetch Back Results
+    final_slope , final_intercept = sess.run([m,b])
+
+
+#Evaluation 
+x_test = np.linspace(-1,11,10)
+y_pred_plot = final_slope*x_test + final_intercept
+
+plt.plot(x_test,y_pred_plot,'r')
+plt.plot(x_data,y_label,'*')    
+
+################################################################################################
---
+++ tensorflow_classification.py
@@ -0,0 +1,74 @@
+import pandas as pd
+diabetes = pd.read_csv('pima-indians-diabetes.csv')
+diabetes.head()
+
+diabetes.columns
+
+cols_to_norm = ['Number_pregnant', 'Glucose_concentration', 'Blood_pressure', 'Triceps',
+       'Insulin', 'BMI', 'Pedigree']
+
+diabetes[cols_to_norm] = diabetes[cols_to_norm].apply(lambda x: (x - x.min()) / (x.max() - x.min()))
+diabetes.head()
+
+
+""" 
+Tf begins 
+
+"""
+import tensorflow as tf
+
+num_preg = tf.feature_column.numeric_column('Number_pregnant')
+plasma_gluc = tf.feature_column.numeric_column('Glucose_concentration')
+dias_press = tf.feature_column.numeric_column('Blood_pressure')
+tricep = tf.feature_column.numeric_column('Triceps')
+insulin = tf.feature_column.numeric_column('Insulin')
+bmi = tf.feature_column.numeric_column('BMI')
+diabetes_pedigree = tf.feature_column.numeric_column('Pedigree')
+age = tf.feature_column.numeric_column('Age')
+
+assigned_group = tf.feature_column.categorical_column_with_vocabulary_list('Group',['A','B','C','D'])
+# Alternative
+# assigned_group = tf.feature_column.categorical_column_with_hash_bucket('Group', hash_bucket_size=10)
+""" If you know the set of all possible feature values of a column and there are only a few of them, you can use categorical_column_with_vocabulary_list.
+ If you don't know the set of possible values in advance you can use categorical_column_with_hash_bucket"""
+
+#Converting Continuous to Categorical
+diabetes['Age'].hist(bins=20)
+age_buckets = tf.feature_column.bucketized_column(age, boundaries=[20,30,40,50,60,70,80])
+feat_cols = [num_preg ,plasma_gluc,dias_press ,tricep ,insulin,bmi,diabetes_pedigree ,assigned_group, age_buckets]
+diabetes.head()
+diabetes.info()
+
+x_data = diabetes.drop('Class',axis=1)
+labels = diabetes['Class']
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(x_data,labels,test_size=0.33, random_state=101)
+
+
+#Input Function
+input_func = tf.estimator.inputs.pandas_input_fn(x=X_train,y=y_train,batch_size=10,num_epochs=1000,shuffle=True)
+
+#Creating the Model
+model = tf.estimator.LinearClassifier(feature_columns=feat_cols,n_classes=2)
+model.train(input_fn=input_func,steps=1000)
+
+#Evaluation
+eval_input_func = tf.estimator.inputs.pandas_input_fn(
+      x=X_test,
+      y=y_test,
+      batch_size=10,
+      num_epochs=1,
+      shuffle=False)
+
+results = model.evaluate(eval_input_func)
+results
+
+#Predictions
+pred_input_func = tf.estimator.inputs.pandas_input_fn(
+      x=X_test,
+      batch_size=10,
+      num_epochs=1,
+      shuffle=False)
+
+
